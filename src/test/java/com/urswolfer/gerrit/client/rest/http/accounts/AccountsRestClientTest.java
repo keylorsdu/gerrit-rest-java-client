f823b53fa7c8aebad4b0999724ad88d36b057b40
keylorsdu <sdukeylor@gmail.com>
2018-08-17 15:13:08 +0800

    implement account create api

--- src/test/java/com/urswolfer/gerrit/client/rest/http/accounts/AccountsRestClientTest.java
+++ src/test/java/com/urswolfer/gerrit/client/rest/http/accounts/AccountsRestClientTest.java
@@ -1,26 +1,27 @@
 /*
  * Copyright 2013-2015 Urs Wolfer
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package com.urswolfer.gerrit.client.rest.http.accounts;
 
-import com.google.gerrit.extensions.common.AccountInfo;
-import com.google.gson.JsonElement;
-import com.urswolfer.gerrit.client.rest.http.GerritRestClient;
 import org.easymock.EasyMock;
 import org.testng.annotations.Test;
+
+import com.google.gerrit.extensions.api.accounts.AccountInput;
+import com.google.gerrit.extensions.common.AccountInfo;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.urswolfer.gerrit.client.rest.http.GerritRestClient;
 
 /**
  * @author Thomas Forrer
@@ -52,11 +53,10 @@ public class AccountsRestClientTest {
 
     @Test
     public void testStarChange() throws Exception {
-        GerritRestClient gerritRestClient = gerritRestClientExpectPut(
-                "/accounts/jdoe/starred.changes/Iccf90a8284f8371a211db9a2824d0617e95a79f9");
-        AccountsRestClient accountsRestClient = new AccountsRestClient(
-                gerritRestClient,
-                EasyMock.createMock(AccountsParser.class));
+        GerritRestClient gerritRestClient =
+            gerritRestClientExpectPut("/accounts/jdoe/starred.changes/Iccf90a8284f8371a211db9a2824d0617e95a79f9");
+        AccountsRestClient accountsRestClient =
+            new AccountsRestClient(gerritRestClient, EasyMock.createMock(AccountsParser.class));
 
         accountsRestClient.id("jdoe").starChange("Iccf90a8284f8371a211db9a2824d0617e95a79f9");
 
@@ -65,11 +65,10 @@ public class AccountsRestClientTest {
 
     @Test
     public void testUnStarChange() throws Exception {
-        GerritRestClient gerritRestClient = gerritRestClientExpectDelete(
-                "/accounts/jdoe/starred.changes/Iccf90a8284f8371a211db9a2824d0617e95a79f9");
-        AccountsRestClient accountsRestClient = new AccountsRestClient(
-                gerritRestClient,
-                EasyMock.createMock(AccountsParser.class));
+        GerritRestClient gerritRestClient =
+            gerritRestClientExpectDelete("/accounts/jdoe/starred.changes/Iccf90a8284f8371a211db9a2824d0617e95a79f9");
+        AccountsRestClient accountsRestClient =
+            new AccountsRestClient(gerritRestClient, EasyMock.createMock(AccountsParser.class));
 
         accountsRestClient.id("jdoe").unstarChange("Iccf90a8284f8371a211db9a2824d0617e95a79f9");
 
@@ -78,11 +77,9 @@ public class AccountsRestClientTest {
 
     @Test
     public void testSuggestAccount() throws Exception {
-        GerritRestClient gerritRestClient = gerritRestClientExpectGet(
-                "/accounts/?suggest&q=jdoe&n=5");
-        AccountsRestClient accountsRestClient = new AccountsRestClient(
-                gerritRestClient,
-                EasyMock.createMock(AccountsParser.class));
+        GerritRestClient gerritRestClient = gerritRestClientExpectGet("/accounts/?suggest&q=jdoe&n=5");
+        AccountsRestClient accountsRestClient =
+            new AccountsRestClient(gerritRestClient, EasyMock.createMock(AccountsParser.class));
 
         accountsRestClient.suggestAccounts("jdoe").withLimit(5).get();
 
@@ -91,33 +88,51 @@ public class AccountsRestClientTest {
 
     private GerritRestClient gerritRestClientExpectGet(String expectedUrl) throws Exception {
         GerritRestClient gerritRestClient = EasyMock.createMock(GerritRestClient.class);
-        EasyMock.expect(gerritRestClient.getRequest(expectedUrl))
-            .andReturn(MOCK_JSON_ELEMENT).once();
+        EasyMock.expect(gerritRestClient.getRequest(expectedUrl)).andReturn(MOCK_JSON_ELEMENT).once();
         EasyMock.replay(gerritRestClient);
         return gerritRestClient;
     }
 
     private GerritRestClient gerritRestClientExpectPut(String expectedUrl) throws Exception {
         GerritRestClient gerritRestClient = EasyMock.createMock(GerritRestClient.class);
-        EasyMock.expect(gerritRestClient.putRequest(expectedUrl))
-            .andReturn(MOCK_JSON_ELEMENT).once();
+        EasyMock.expect(gerritRestClient.putRequest(expectedUrl)).andReturn(MOCK_JSON_ELEMENT).once();
         EasyMock.replay(gerritRestClient);
         return gerritRestClient;
     }
 
     private GerritRestClient gerritRestClientExpectDelete(String expectedUrl) throws Exception {
         GerritRestClient gerritRestClient = EasyMock.createMock(GerritRestClient.class);
-        EasyMock.expect(gerritRestClient.deleteRequest(expectedUrl))
-            .andReturn(MOCK_JSON_ELEMENT).once();
+        EasyMock.expect(gerritRestClient.deleteRequest(expectedUrl)).andReturn(MOCK_JSON_ELEMENT).once();
         EasyMock.replay(gerritRestClient);
         return gerritRestClient;
     }
 
     private AccountsParser getAccountsParser() throws Exception {
         AccountsParser accountsParser = EasyMock.createMock(AccountsParser.class);
-        EasyMock.expect(accountsParser.parseAccountInfo(MOCK_JSON_ELEMENT))
-                .andReturn(MOCK_ACCOUNT_INFO).once();
+        EasyMock.expect(accountsParser.parseAccountInfo(MOCK_JSON_ELEMENT)).andReturn(MOCK_ACCOUNT_INFO).once();
         EasyMock.replay(accountsParser);
         return accountsParser;
+    }
+
+    @Test
+    public void testCreate() throws RestApiException {
+        AccountInput input = new AccountInput();
+        input.username = "john";
+        Gson gson = new Gson();
+        // Mock Account Parser's work
+        AccountsParser accountsParser = EasyMock.createMock(AccountsParser.class);
+        EasyMock.expect(accountsParser.getAccountInputJson(input)).andReturn(gson.toJson(input)).times(2);
+        EasyMock.expect(accountsParser.parseAccountInfo(MOCK_JSON_ELEMENT)).andReturn(MOCK_ACCOUNT_INFO).once();
+        EasyMock.replay(accountsParser);
+        // Mock Gerrit Client's work
+        GerritRestClient gerritRestClient = EasyMock.createMock(GerritRestClient.class);
+        EasyMock.expect(gerritRestClient.putRequest("/accounts/john", accountsParser.getAccountInputJson(input)))
+            .andReturn(MOCK_JSON_ELEMENT).once();
+        EasyMock.replay(gerritRestClient);
+        AccountsRestClient accountsRestClient = new AccountsRestClient(gerritRestClient, accountsParser);
+
+        accountsRestClient.create(input);
+
+        EasyMock.verify(gerritRestClient);
     }
 }
